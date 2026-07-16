63a3eca16bb9c134d4b72f0a8233b9d965e809fe
Demyd Titenko <demidd06@gmail.com>
2026-07-16 14:19:42 +0300

    feat: added global exception hendler to this project

---
+++ src/main/java/com/example/todolist/dto/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.example.todolist.dto;
+
+/**
+ *
+ * Created by: Demyd Titenko
+ * <p>
+ * Date: 15.07.2026
+ * <p>
+ * Time: 20:31
+ * <p>
+ * Project name: ToDoList
+ */
+public record ErrorResponse(
+        String message
+) {
+}
---
+++ src/main/java/com/example/todolist/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.todolist.exception;
+
+import com.example.todolist.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ *
+ * Created by: Demyd Titenko
+ * <p>
+ * Date: 15.07.2026
+ * <p>
+ * Time: 20:33
+ * <p>
+ * Project name: ToDoList
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationExceptions(MethodArgumentNotValidException e){
+        return new ErrorResponse(e.getBindingResult().getFieldError().getDefaultMessage());
+    }
+}
