efad484b8ae096037267118354202ff449e581ff
Demyd Titenko <demidd06@gmail.com>
2026-07-16 14:19:12 +0300

    fix: fix application properties and TaskService logic for working DB

--- src/main/java/com/example/todolist/service/TaskService.java
+++ src/main/java/com/example/todolist/service/TaskService.java
@@ -2,6 +2,9 @@ package com.example.todolist.service;
 
 import com.example.todolist.dto.TaskCreateRequest;
 import com.example.todolist.dto.TaskResponse;
+import com.example.todolist.entity.Task;
+import com.example.todolist.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,8 +18,21 @@ import org.springframework.stereotype.Service;
  * Project name: ToDoList
  */
 @Service
+@RequiredArgsConstructor
 public class TaskService {
+    private final TaskRepository taskRepository;
+
     public TaskResponse createTask(TaskCreateRequest taskCreateRequest){
-        return new TaskResponse("TaskID-1", taskCreateRequest.title(), "CREATED");
+        Task task = new Task();
+        task.setTitle(taskCreateRequest.title());
+        task.setDescription(taskCreateRequest.description());
+        task.setStatus("CREATED");
+
+        Task saveTask = taskRepository.save(task);
+        return new TaskResponse(
+                saveTask.getId().toString(),
+                saveTask.getTitle(),
+                saveTask.getStatus()
+        );
     }
 }
