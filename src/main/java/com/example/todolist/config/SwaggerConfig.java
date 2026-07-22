109fad8a1795f1dd9a0c67c15079eeda11ac7d6c
Demyd Titenko <demidd06@gmail.com>
2026-07-22 13:07:38 +0300

    feat: add JWT support to Swagger

---
+++ src/main/java/com/example/todolist/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.example.todolist.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * Created by: Demyd Titenko
+ * <p>
+ * Date: 22.07.2026
+ * <p>
+ * Time: 12:55
+ * <p>
+ * Project name: ToDoList
+ */
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
